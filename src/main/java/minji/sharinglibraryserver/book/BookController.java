47fd3a39cbb8b59bfb187cbb9701f9090b0a17de
minji1110 <kalswl991110@gmail.com>
2021-08-20 01:30:11 +0900

    책 단권저장 api

--- src/main/java/minji/sharinglibraryserver/book/BookController.java
+++ src/main/java/minji/sharinglibraryserver/book/BookController.java
@@ -1,32 +1,19 @@
 package minji.sharinglibraryserver.book;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpHeaders;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
-    private static final String KAKAO_HOST ="https://dapi.kakao.com";
-    private static final String KAKAO_SEARCH_URI="/v3/search/book?target=title";
 
-//    @GetMapping("/book")
-//    public ResponseEntity searchBook(String searchVal){
-//        RestTemplate restTemplate=new RestTemplate();
-//        HttpHeaders httpHeaders=new HttpHeaders();
-
-//        httpHeaders.add()
-//        return
-//    }
+    @PostMapping(value="/book/{userId}")
+    public Book saveBook(@PathVariable long userId, @ModelAttribute KAKAOBookSearchDto kakaoBookSearchDto){
+        return bookService.saveBook(userId,kakaoBookSearchDto);
+    }
 
     @GetMapping(value = "/books/{userId}")
     public List<Book> getBookList(@PathVariable long userId){
--- src/main/java/minji/sharinglibraryserver/book/BookService.java
+++ src/main/java/minji/sharinglibraryserver/book/BookService.java
@@ -1,5 +1,6 @@
 package minji.sharinglibraryserver.book;
 
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import minji.sharinglibraryserver.user.User;
 import minji.sharinglibraryserver.user.UserJpaRepo;
@@ -15,6 +16,22 @@ public class BookService {
     private final UserJpaRepo userJpaRepo;
     private final BookJpaRepo bookJpaRepo;
 
+
+    //책 저장
+    public Book saveBook(long userId, KAKAOBookSearchDto kakaoBookSearchDto){
+        User user=userJpaRepo.getById(userId);
+        Book book= Book.builder()
+                .bookTitle(kakaoBookSearchDto.title)
+                .bookAuthor(kakaoBookSearchDto.authors)
+                .bookIsbn(kakaoBookSearchDto.isbn)
+                .bookSummary(kakaoBookSearchDto.contents)
+                .user(user)
+                .build();
+
+        return bookJpaRepo.save(book);
+    }
+
+    //사용자별 책 목록 조회
     public List<Book> getBooksByUser(long userId){
         User user=userJpaRepo.getById(userId);
         List<Book> bookList=bookJpaRepo.findBooksByUser(user);
