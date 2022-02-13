3c1ebd9f18ae88e31dc787130d5f50883cefe064
minji1110 <kalswl991110@gmail.com>
2022-02-13 15:38:57 +0900

    UriComponentsBuilder 사용

--- src/main/java/minji/sharinglibraryserver/kakao/KakaoController.java
+++ src/main/java/minji/sharinglibraryserver/kakao/KakaoController.java
@@ -9,20 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 @Slf4j
 @RestController
 public class KakaoController {
     private static final String host="https://dapi.kakao.com";
-    private static final String searchTitleUri="/v3/search/book?target=title&query=";
+    private static final String searchTitlePath ="/v3/search/book";
     private static final String apiKey="1345794fdd4b8c268cc7ff2b244e7dbf";  //fixme 비공개처리
 
     @GetMapping(value="/book/search")
-    public KakaoBookResult searchBook(@RequestParam String title) throws URISyntaxException, JsonProcessingException {
-        URI kakaoAPI=new URI(host+searchTitleUri+title);
+    public KakaoBookResult searchBook(@RequestParam String title) throws  JsonProcessingException {
+        URI kakaoAPI = UriComponentsBuilder
+                .fromUriString(host)
+                .path(searchTitlePath)
+                .queryParam("target","title")
+                .queryParam("query",title)
+                .build()
+                .toUri();
+        System.out.println("kakaoAPI = " + kakaoAPI);
 
         HttpHeaders httpHeaders=new HttpHeaders();
         httpHeaders.set("Authorization","KakaoAK "+apiKey);
@@ -35,6 +41,7 @@ public class KakaoController {
                 httpEntity,
                 String.class
         );
+
 
         ObjectMapper objectMapper=new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
         KakaoBookResult kakaoBookResult =objectMapper.readValue(result.getBody(), KakaoBookResult.class);
